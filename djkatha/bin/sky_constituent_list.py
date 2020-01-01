3048886fb7147d0b1bde7ca722db47865fff8c99
dsullivan <dsullivan@carthage.edu>
2020-01-01 11:00:55 -0600

    working on sql

--- djkatha/bin/sky_constituent_list.py
+++ djkatha/bin/sky_constituent_list.py
@@ -7,20 +7,19 @@ Python functions to
     based on tokens in the files.
 """
 
-# from pathlib import Path
-import requests
+# import requests
 import sys
 import os
-import json
-import time
-# import base64
-import datetime as dt
 import django
-import os.path
-import csv
+import argparse
+import pyodbc
+import datetime
+from datetime import datetime, timezone
+from datetime import date
+import time
+import arrow
+from time import strftime, strptime
 
-# import cryptography
-from datetime import datetime
 
 # Note to self, keep this here
 # django settings for shell environment
@@ -28,15 +27,39 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djkatha.settings.shell")
 django.setup()
 # ________________
 from django.conf import settings
-from django.core.cache import cache
-from djkatha.core.sky_api_auth import token_refresh
-from djkatha.core.sky_api_calls import get_constituent_list
+from djkatha.core.sky_api_auth import fn_do_token
+from djkatha.core.sky_api_calls import get_constituent_list, \
+    get_constituent_custom_fields
+from djimix.core.utils import get_connection, xsql
 
-    # get_const_custom_fields, api_get, \
-    # get_constituent_id, set_const_custom_field, update_const_custom_fields, \
-    # delete_const_custom_fields, get_relationships, api_post, api_patch, \
-    # api_delete, get_custom_fields, get_custom_field_value, get_constituent_list
 
+# informix environment
+os.environ['INFORMIXSERVER'] = settings.INFORMIXSERVER
+os.environ['DBSERVERNAME'] = settings.DBSERVERNAME
+os.environ['INFORMIXDIR'] = settings.INFORMIXDIR
+os.environ['ODBCINI'] = settings.ODBCINI
+os.environ['ONCONFIG'] = settings.ONCONFIG
+os.environ['INFORMIXSQLHOSTS'] = settings.INFORMIXSQLHOSTS
+
+
+# normally set as 'debug" in SETTINGS
+DEBUG = settings.INFORMIX_DEBUG
+desc = """
+    Collect data from Blackbaud
+"""
+parser = argparse.ArgumentParser(description=desc)
+
+parser.add_argument(
+    "--test",
+    action='store_true',
+    help="Dry run?",
+    dest="test"
+)
+parser.add_argument(
+    "-d", "--database",
+    help="database name.",
+    dest="database"
+)
 """
     12/9/19 - The API call to get_constituent_list can be filtered by student
       status and add date.   
@@ -50,100 +73,258 @@ from djkatha.core.sky_api_calls import get_constituent_list
       The process would have to involve finding the status of active students
       in CX, (Look for a change date...to limit the number.  Maybe audit table)
     
-      Then determine if the student is in Raiser's Edge by readint the list
+      Then determine if the student is in Raiser's Edge by reading the list
       just retrieved. 
-      If not add student, 
-         then add the custom field record
+
+        Currently the student adds would be periodic
+        OMatic processes, but I would possibly need to create the custom field
+        Record if OMatic doesn't create it 
+        
+        May be easiest to just purge the table and repopulate it periodically
+
+      If not add student  ??,
+          then add the custom field record  ???
       Else - find out of custom field record exists
-        If not add
-        else update
+          If not add
+          else update
 
       So each student will require 1-2 API calls
     
       No way to test any of this because there are no students in RE yet...
 """
 
+
+def fn_convert_date(date):
+    # print(date)
+    if date != "":
+        '%Y-%m-%dT%H:%M:%S%z'
+        # timestamp = date.replace(tzinfo=timezone.utc).timestamp()
+        # print(timestamp)
+        # ndate = datetime.strptime(date, "%Y-%m-%dT%H:%M:%S.%f")
+        # ndate = datetime.strptime(date, "%Y-%m-%d %H:M:%S.%f-z")
+
+        # Use tool calld Arrow, because BB date formats are a mess
+        # creates an Arrow object
+        ndate = arrow.get(date)
+        # print(ndate)
+
+        # arrow has a datetime for its arrow object
+        dt = ndate.datetime
+
+        #Now I have something I can format
+        # retdate = datetime.strftime(dt, "%Y-%m-%d")
+        retdate = datetime.strftime(dt, "%m/%d/%Y")
+        # print(retdate)
+    else:
+        retdate = ''
+    # print(str(date) + ',' + str(retdate))
+    return retdate
+
+
+def fn_insert_local(i):
+
+    try:
+        if i['type'] == 'Individual':
+            dt_add = fn_convert_date((i['date_added']))
+            dt_mod = fn_convert_date((i['date_modified']))
+            fullname = i['first'].strip() + ' ' + i['last'].strip()
+            print(dt_mod)
+
+            q_ins_sql = '''INSERT INTO cx_sandbox:raisers_edge_id_match 
+                (id, re_id, fullname, category, const_type) 
+                VALUES(?, ?, ?, ?, ?)'''
+
+            q_ins_args = (i['lookup_id'], i['id'], fullname, 'Student Status',
+                          i['type'])
+
+            print(q_ins_sql)
+            print(q_ins_args)
+            connection = get_connection(EARL)
+            with connection:
+                cur = connection.cursor()
+                cur.execute(q_ins_sql, q_ins_args)
+                # connection.commit()
+
+    except pyodbc.Error as err:
+        # print("Error in fn_insert_local:  " + str(err))
+        sqlstate = err.args[0]
+        print(sqlstate)
+
+
+def fn_update_local(i):
+    try:
+        bb_id = i['id']
+        carth_id = i['lookup_id']
+        name = i['first'].strip() + ' ' + i['last'].strip()
+        type = i['type']
+        dt_add = fn_convert_date((i['date_added']))
+        dt_mod = fn_convert_date((i['date_modified']))
+        print(dt_mod)
+        print(dt_add)
+
+        if type == 'Individual':
+            print('UPDATE: Name = ' + name + ', CarthID = ' + str(carth_id)
+            + ', BlackbaudID = ' + str(bb_id) + ', type = '
+            + type)
+        q_upd_sql = '''UPDATE cx_sandbox:raisers_edge_id_match
+                SET re_id = ?, fullname = ?, category = ?, const_type = ?, 
+                date_added = ?
+                WHERE id = ?
+                '''
+        q_upd_args = (bb_id, name, 'Student Status', type, carth_id, dt_add)
+        print(q_upd_args)
+        connection = get_connection(EARL)
+        print(q_upd_sql)
+        with connection:
+            cur = connection.cursor()
+            cur.execute(q_upd_sql, q_upd_args)
+    except pyodbc.Error as err:
+            # print("Error in fn_update_local:  " + str(err))
+            sqlstate = err.args[0]
+            print(sqlstate)
+
 def main():
     try:
+        # set global variable
+        global EARL
 
-        action = 'get_constituent_list'
-
-        """--------REFRESH THE TOKEN------------------"""
-        """ Because the token only lasts for 60 minutes when things are idle
-            it will be necessary to refresh the token before attempting
-            anything else.   The refresh token will be valid for 60 days,
-            so it should return a new token with no problem.  All the API
-            calls will get new tokens, resetting the 60 minute clock, 
-            so to avoid calling for a token every time, I may have to 
-            either set a timer or see if I can read the date and time from the
-            cache files and compare to current time
-         """
-
-        """Check to see if the token has expired, if so refresh it
-            the token expires in 60 minutes, but the refresh token
-            is good for 60 days"""
-        t = cache.get('refreshtime')
-        print(t)
-
-        if t is None:
-            r = token_refresh()
-            print(r)
-        elif t < datetime.now() - dt.timedelta(minutes=59):
-            print('Out of limit')
-            print(t)
-            print(datetime.now() - dt.timedelta(minutes=59))
-            r = token_refresh()
-            print(r)
-        else:
-            print("within limit")
-
+        # determines which database is being called from the command line
+        # if database == 'cars':
+        #     EARL = settings.INFORMIX_ODBC_TRAIN
+        # if database == 'train':
+        EARL = settings.INFORMIX_ODBC_TRAIN
+        # if database == 'sandbox':
+        #     EARL = settings.INFORMIX_ODBC_SANDBOX
 
         """"--------GET THE TOKEN------------------"""
-        current_token = cache.get('tokenkey')
+        current_token = fn_do_token()
         # print("Current Token = ")
         # print(current_token)
 
+
         """Here is a question:  There will be over 100k entries in their
             system.  How can I retrieve only students?"""
+        """-----Get a list of constituents-------"""
+        """---May need this to match Carthage ID to Blackbaud ID------"""
+        x = get_constituent_list(current_token)
+        for i in x['value']:
+            # print(x)
+            #     print(i['id'])
+            carth_id = i['lookup_id']
 
-        if action == 'get_constituent_list':
-            """-----Get a list of constituents-------"""
-            """---May need this to match Carthage ID to Blackbaud ID------"""
-            ret = get_constituent_list(current_token)
-            print(ret)
 
-        # """-----Once done, the token must be refreshed-------"""
-        # Changed this.   Test at top to see if token has expired, then
-        # Refresh the API tokens
-        # r = token_refresh()
-        # print(r)
+            """Will write the BB ID to a table - not yet created
+                will be in a mysql database...for now in CX sandbox
+            """
+            # q_ins_wc = '''
+            # INSERT  INTO
+            # cc_work_cat_table(work_cat_code, work_cat_descr,
+            #                   active_date)
+            # VALUES(?, ?, ?)'''
+            #     q_ins_wc_args = (workercatcode,workercatdescr,
+            #                      datetime.now().strftime("%m/%d/%Y"))
+            # engine.execute(q_ins_wc, q_ins_wc_args)
+            print(carth_id)
+
+            chk_sql = '''select count(id) from
+                cx_sandbox:raisers_edge_id_match
+                where id = {}'''.format(carth_id)
+            print(chk_sql)
+            connection = get_connection(EARL)
+            with connection:
+                data_result = xsql(chk_sql)
+                # print("Query result = " + str(data_result))
+                if data_result:
+                    for row in data_result:
+                        if row[0] == 1:
+                            print("Record exists for " + str(carth_id))
+                            print("Update")
+                            fn_update_local(i)
+                        elif row[0] == 0:
+                            print("No record for " + str(carth_id))
+                            print("Insert")
+                            fn_insert_local(i)
+                        elif row[0] > 1:
+                            print("Error - multiple records for " + str(carth_id))
+                else:
+                    print("No record for " + str(carth_id))
+                    print("Insert")
+                    fn_insert_local(i)
+
+                #     connection.cursor.close()
+
+                    # if data_result == 0 or data_result is None:
+                    #     print("insert")
+                    # else:
+                    #     print("update")
+
+                # # For testing and development...
+                # ins_sql = '''INSERT INTO cx_sandbox:raisers_edge_id_match
+                #            (id, re_id, fullname, category, value, date_added,
+                #            date_updated, comment)
+                #            VALUES(?,?,?,?,?,?,?,?)'''
+                # ins_args = (carth_id, bb_id, name, 'Student Status', type,
+                #             '2019-11-13', '2019-11-21', 'Testing an add')
+                #
+                # print(ins_sql)
+                # print(ins_args)
+
+                # try:
+                #     cursor = connection.cursor()
+                #     # print(connection)
+                #     cursor.execute(ins_sql, ins_args)
+                #     # with connection:
+                #     #     xsql(ins_sql, ins_args)
+                #
+                # # except connection.cursor.IntegrityError as e:
+                # except Exception as e:
+                #     print("Error {} ".format(e) )
+                # # print(str(sys.exc_info()[:2]))
+
+        # with connection:
+        #     data_result = xsql(
+        #         ins_sql, connection,
+        #         key=settings.INFORMIX_DEBUG
+        #     ).fetchall()
+
+        # ret = list(data_result)
+        # for i in ret:
+        #     print(str(i[0]) + " " + i[1] + " " + i[5])
+
+
 
     except Exception as e:
-        print("Error in main:  " + e.message)
+        print("Error in main:  " + str(e))
+        # print(type(e))
+        # print(e.args)
+        sqlstate = e.args[1]
+        print(sqlstate)
+
+        # print(str(e.message))
         # fn_write_error("Error in misc_fees.py - Main: "
-        #                + e.message)
+        #                + str(e))
 
 
 if __name__ == "__main__":
-    # args = parser.parse_args()
-    # test = args.test
-    # database = args.database
+    args = parser.parse_args()
+    test = args.test
+    database = args.database
 
-    # if not database:
-    #     print "mandatory option missing: database name\n"
-    #     parser.print_help()
-    #     exit(-1)
-    # else:
-    #     database = database.lower()
+    if not database:
+        print("mandatory option missing: database name\n")
+        parser.print_help()
+        exit(-1)
+    else:
+        database = database.lower()
 
-    # if database != 'cars' and database != 'train' and database != 'sandbox':
-    #     print "database must be: 'cars' or 'train' or 'sandbox'\n"
-    #     parser.print_help()
-    #     exit(-1)
-    #
-    # if not test:
-    #     test = 'prod'
-    # else:
-    #     test = "test"
+    if database != 'cars' and database != 'train' and database != 'sandbox':
+        print("database must be: 'cars' or 'train' or 'sandbox'\n")
+        parser.print_help()
+        exit(-1)
+
+    if not test:
+        test = 'prod'
+    else:
+        test = "test"
 
     sys.exit(main())
