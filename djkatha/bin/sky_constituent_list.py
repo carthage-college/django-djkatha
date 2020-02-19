879d4a3f7311710df520fcdeba4c445d36b44990
dsullivan <dsullivan@carthage.edu>
2020-02-19 15:30:41 -0600

    A lot of cleanup

--- djkatha/bin/sky_constituent_list.py
+++ djkatha/bin/sky_constituent_list.py
@@ -14,7 +14,6 @@ import argparse
 import pyodbc
 import datetime
 from datetime import datetime, date, timedelta
-import arrow
 from time import strftime, strptime
 import django
 
@@ -26,11 +25,9 @@ django.setup()
 # ________________
 from django.conf import settings
 from djkatha.core.sky_api_auth import fn_do_token
-from djkatha.core.sky_api_calls import get_constituent_list, \
-    get_constituent_custom_fields, get_constituents_custom_field_list, \
-    get_constituent_id, get_lookup_id
+from djkatha.core.sky_api_calls import get_constituents_custom_field_list, \
+    get_lookup_id
 from djimix.core.utils import get_connection, xsql
-
 
 # informix environment
 os.environ['INFORMIXSERVER'] = settings.INFORMIXSERVER
@@ -94,91 +91,8 @@ parser.add_argument(
 """
 
 
-def fn_convert_date(date):
-    # print(date)
-    if date != "":
-        '%Y-%m-%dT%H:%M:%S%z'
 
-        # Use tool calld Arrow, because BB date formats are a mess
-        # creates an Arrow object
-        ndate = arrow.get(date)
-        print(ndate)
-
-        # arrow has a datetime for its arrow object
-        dt = ndate.datetime
-
-        #Now I have something I can format
-        retdate = datetime.strftime(dt, "%m/%d/%Y")
-        print(retdate)
-    else:
-        retdate = ''
-    # print(str(date) + ',' + str(retdate))
-    return retdate
-
-
-# def fn_insert_local(i):
-#
-#     try:
-#         if i['type'] == 'Individual':
-#             fullname = i['first'].strip() + ' ' + i['last'].strip()
-#             id = i['lookup_id']
-#             bbid = i['id']
-#             typ = i['type']
-#
-#             q_ins_sql = '''INSERT INTO cvid_rec
-#                 (id, re_id, fullname, category, const_type)
-#                 VALUES(?, ?, ?, ?, ?)'''
-#
-#             q_ins_args = (id, bbid,  fullname, 'Student Status',
-#                           typ)
-#
-#             print(q_ins_sql)
-#             print(q_ins_args)
-#             # connection = get_connection(EARL)
-#             # with connection:
-#             #     cur = connection.cursor()
-#             #     cur.execute(q_ins_sql, q_ins_args)
-#             #     # connection.commit()
-#
-#     except pyodbc.Error as err:
-#         # print("Error in fn_insert_local:  " + str(err))
-#         sqlstate = err.args[0]
-#         print(sqlstate)
-
-
-def fn_update_local(i):
-    try:
-        bb_id = i['id']
-        carth_id = i['lookup_id']
-        name = i['first'].strip() + ' ' + i['last'].strip()
-        type = i['type']
-
-        # if type == 'Individual':
-        #     print('UPDATE: Name = ' + name + ', CarthID = ' + str(carth_id)
-        #     + ', BlackbaudID = ' + str(bb_id) + ', type = '
-        #     + type)
-        q_upd_sql = '''UPDATE cvid_rec
-                SET re_api_id = ? WHERE cx_id = ?
-                '''
-        q_upd_args = (bb_id, carth_id)
-        # q_upd_sql = '''UPDATE raisers_edge_id_match
-        #         SET re_id = ?, fullname = ?, category = ?, const_type = ?
-        #         WHERE id = ?
-        #         '''
-        # q_upd_args = (bb_id, name, 'Student Status', type, carth_id)
-        # print(q_upd_args)
-        connection = get_connection(EARL)
-        print(q_upd_sql)
-        with connection:
-            cur = connection.cursor()
-            cur.execute(q_upd_sql, q_upd_args)
-    except pyodbc.Error as err:
-            # print("Error in fn_update_local:  " + str(err))
-            sqlstate = err.args[0]
-            print(sqlstate)
-
-
-def fn_update_local2(carth_id, bb_id):
+def fn_update_local(carth_id, bb_id):
     try:
 
         q_upd_sql = '''UPDATE cvid_rec
@@ -257,7 +171,7 @@ def main():
                         #       + str(bb_id))
                         carth_id = get_lookup_id(current_token, bb_id)
                         # print(bb_id)
-                        ret = fn_update_local2(carth_id, bb_id)
+                        ret = fn_update_local(carth_id, bb_id)
                         # print(ret)
                     else:
                         # print("CVID Rec exists for" + str(x[0]))
@@ -271,98 +185,6 @@ def main():
         # print(e.args)
         sqlstate = e.args[1]
         print(sqlstate)
-
-        # print(str(e.message))
-        # fn_write_error("Error in misc_fees.py - Main: "
-        #                + str(e))
-
-
-# def main():
-#     try:
-#         # set global variable
-#         global EARL
-#
-#         # determines which database is being called from the command line
-#         # if database == 'cars':
-#         #     EARL = settings.INFORMIX_ODBC_TRAIN
-#         # if database == 'train':
-#         EARL = settings.INFORMIX_ODBC_TRAIN
-#         # if database == 'sandbox':
-#         #     EARL = settings.INFORMIX_ODBC_SANDBOX
-#
-#         """"--------GET THE TOKEN------------------"""
-#         current_token = fn_do_token()
-#         # print("Current Token = ")
-#         # print(current_token)
-#
-#         # print(EARL)
-#         """-----Get a list of constituents with a custom field of
-#             Student Status-------"""
-#         """---We need this to match Carthage ID to Blackbaud ID------"""
-#
-#         searchtime = date.today() + timedelta(days=-10)
-#         print("Searchtime = " + str(searchtime))
-#
-#         x = get_constituent_list(current_token, str(searchtime))
-#         if x == 0:
-#             print("No Data Returned")
-#         else:
-#             for i in x['value']:
-#                 # print(x)
-#                 #     print(i['id'])
-#                 carth_id = i['lookup_id']
-#
-#                 print("Date Added = " + str(i['date_added']))
-#
-#                 """Will write the BB ID to a table - not yet created
-#                     will be in a mysql database...for now in CX sandbox
-#                 """
-#                 # print(carth_id)
-#
-#                 chk_sql = '''select cx_id, re_api_id from cvid_rec
-#                     where cx_id = {}'''.format(carth_id)
-#
-#                 # print(chk_sql)
-#                 connection = get_connection(EARL)
-#
-#                 with connection:
-#                     data_result = xsql(chk_sql, connection, key='debug')
-#                     if data_result:
-#                         for row in data_result:
-#                             # print(str(row[1]))
-#                             if row[1] is None:
-#                                 print("Add bb_id for " + str(row[0]))
-#                                 # fn_update_local(i)
-#                             else:
-#                                 print("bbid = " + str(i['id']))
-#                                 # print("CX Value = " + str(row[1]))
-#
-#                             # if row[1] == 1:
-#                             #     print("Record exists for " + str(carth_id))
-#                             #     # should check to see if it matches?
-#                             #     print("Update")
-#                             #     fn_update_local(i)
-#                             # elif row[1] == 0:
-#                             #     print("No record for " + str(carth_id))
-#                             #     print("Insert")
-#                             #     fn_update_local(i)
-#                             # elif row[1] > 1:
-#                             #     print("Error - multiple records for " + str(carth_id))
-#                     else:
-#                         print("No cvid record for " + str(carth_id))
-#                         # print("Insert")
-#                         # fn_insert_local(i)
-#
-#     except Exception as e:
-#         print("Error in main:  " + str(e))
-#         # print(type(e))
-#         # print(e.args)
-#         sqlstate = e.args[1]
-#         print(sqlstate)
-#
-#         # print(str(e.message))
-#         # fn_write_error("Error in misc_fees.py - Main: "
-#         #                + str(e))
 
 
 if __name__ == "__main__":
