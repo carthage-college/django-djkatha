ddab9d5a6a0eac6dd389848bdd6727409f69fcde
plungerman <plungerman@gmail.com>
2020-11-17 18:42:22 -0600

    code clean up as a means to study of API

--- djkatha/bin/initial_auth.py
+++ djkatha/bin/initial_auth.py
@@ -39,7 +39,6 @@ def get_initial_token():
            " 'https://127.0.0.1/?code=xxxx'. Copy the value of the code "
            "(after the '=' sign). "
            "Paste that code into the prompt below.")
-
     print("---  {0}  ---".format(authorization_redirect_url))
     authorization_code = input("Paste code here: ")
 
@@ -56,23 +55,23 @@ def get_initial_token():
         },
     )
 
-    # For Cryptography,
-    # This only needs to happen once...store and re-use
-    key = fernet.Fernet.generate_key()
-
     tokens_dict = dict(json.loads(ref_token_getter.text))
+
     print(tokens_dict)
-    frn = fernet.Fernet(key)
-    print(frn)
     refresh_token = tokens_dict['refresh_token']
     access_token = tokens_dict['access_token']
 
     cache.set('tokenkey', access_token)
 
-    x = cache.get('tokenkey')
-    y = cache.get('refreshkey')
-    print(x)
-    print(y)
+    print('token type = {0}'.format(tokens_dict['token_type']))
+    print('access token = {0}'.format(cache.get('tokenkey')))
+    print('access token expires in = {0} seconds (1 hour)'.format(
+        tokens_dict['expires_in'],
+    ))
+    print('refresh token = {0}'.format(refresh_token))
+    print('refresh token expires in = {0} seconds (1 year)'.format(
+        tokens_dict['refresh_token_expires_in'],
+    ))
 
     return 1
 
